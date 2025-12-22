dd1a4d97e949e646a7565322ac124d93839eca53
Aakarsh Singh <singhaakarsh1506@gmail.com>
2025-12-22 20:51:16 +0530

    Props in React.js

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,7 +3,9 @@ import Students from './Students.jsx'
 function App(){
    return(
       <>
-        <Students name="Aakarsh Singh" />
+        <Students name="Aakarsh Singh" age={18} isStudent={true}/>
+        <Students name="Aashita Singh" age={13} isStudent={true}/>
+        <Students />
       </>
    );
 }
