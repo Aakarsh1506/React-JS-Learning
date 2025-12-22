ffc8778f7a06fcc275bf3853ad683baac4bd7afc
Aakarsh Singh <singhaakarsh1506@gmail.com>
2025-12-22 17:11:03 +0530

    Card-Components in React.js

--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -5,7 +5,7 @@ function Card(){
         <div className="Card">
             <img className="Card-Image" src={profilePic} alt="Profile Picture" width="150" height="150"></img>
             <h2 className="Card-Title">Aakarsh Singh</h2>
-            <p className="Card-Desc">First Year B.Tech IT Student<br/> Hobby: Playing Football</p>
+            <p className="Card-Desc">First Year B.Tech IT Student<br/> Hobby: Gaming</p>
         </div>
     );
 }
