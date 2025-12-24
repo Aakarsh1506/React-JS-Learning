5c760e50c45ce8e4942238f10b0263ba8171a190
Aakarsh Singh <singhaakarsh1506@gmail.com>
2025-12-24 15:18:18 +0530

    List Rendering Part-2 in React.js

--- my-react-app/src/App2.jsx
+++ my-react-app/src/App2.jsx
@@ -1,8 +1,15 @@
 import List from './List.jsx'
 
 function App2(){
+    
+     const fruits = [{id: 1, name: "Apple", calories: 95},
+                    {id: 2, name: "Orange", calories: 45},
+                    {id: 3, name: "Banana", calories: 105}, 
+                    {id: 4, name: "Pineapple", calories: 37}, 
+                    {id: 5, name: "Mango", calories: 99}];
+    
     return(
-        <List></List>
+        <List items={fruits} category="Fruits"></List>
     );
 }
 
--- my-react-app/src/List.jsx
+++ my-react-app/src/List.jsx
@@ -1,19 +1,26 @@
 
-function List(){
-    const fruits = [{id: 1, name: "Apple", calories: 95},
-                    {id: 2, name: "Orange", calories: 45},
-                    {id: 3, name: "Banana", calories: 105}, 
-                    {id: 4, name: "Pineapple", calories: 37}, 
-                    {id: 5, name: "Mango", calories: 99}];
-    
-    fruits.sort((a,b) => a.name.localeCompare(b.name)); //Sort logic for name
+function List(props){
+   
+    const itemsList = props.items; //Printing the list using props
+    const category = props.category;
+    //fruits.sort((a,b) => a.name.localeCompare(b.name)); //Sort logic for name
     //fruits.sort((a,b) => a.calories - b.calories) //Sort logic for calories low to high
     //fruits.sort((a,b) => b.calories - a.calories) //Sort logic for calories high to low
+    
+    //const Fruits = fruits.filter(fruits => fruits.calories); //Filtering low calorie fruits using filter function
+    //const lowCalFruits = fruits.filter(fruits => fruits.calories<100); //Filtering low calorie fruits using filter function
+    //const highCalFruits = fruits.filter(fruits => fruits.calories>100); //Filtering high calorie fruits using filter function
 
-    const listItems = fruits.map(fruits => <li key={fruits.id}>{fruits.name}: &nbsp; {fruits.calories}cal</li>); //&nbsp; is a space creater
+
+    const listItems = itemsList.map(item=> <li key={item.id}>{item.name}: &nbsp; {item.calories}cal</li>); //&nbsp; is a space creater
+
+    //const listItems = lowCalFruits.map(lowCalFruits=> <li key={lowCalFruits.id}>{lowCalFruits.name}: &nbsp; {lowCalFruits.calories}cal</li>); //&nbsp; is a space creater
+    //const listItems = highCalFruits.map(highCalFruits=> <li key={highCalFruits.id}>{highCalFruits.name}: &nbsp; {highCalFruits.calories}cal</li>); //&nbsp; is a space creater
+
 
     return(
            <>
+            <h3>{category}</h3>
             <ul>{listItems}</ul>
            </>
     );
